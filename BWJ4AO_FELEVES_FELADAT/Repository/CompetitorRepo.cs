27029d14652bc998d5d647b9e1ad7df822f5ac58
Bence Papp <bencepapp1101@gmail.com>
2020-11-17 16:20:11 +0100

    Repository réteg létrehozva, CompetitorRepo elkészítve

---
+++ BWJ4AO_FELEVES_FELADAT/Repository/CompetitorRepo.cs
@@ -0,0 +1,69 @@
+﻿using Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+      public class CompetitorRepo : IRepository<Competitor>
+      {
+            CompetitorDbContext cont = new CompetitorDbContext();
+
+            public void AddNewItem(Competitor item)
+            {
+                  cont.Competitors.Add(item);
+                  cont.SaveChanges();
+            }
+
+            public void Delete(Competitor item)
+            {
+                  cont.Competitors.Remove(item);
+                  cont.SaveChanges();
+            }
+
+            public void Delete(int number)
+            {
+                  Delete(FindOne(number));
+            }
+
+            public Competitor FindOne(int number)
+            {
+                  var q = (from x in cont.Competitors
+                           where x.Number == number
+                           select x).FirstOrDefault();
+
+                  return q;
+            }
+
+            public IQueryable<Competitor> ListAll()
+            {
+                  return cont.Competitors.AsQueryable();
+            }
+
+            public void SaveAll()
+            {
+                  cont.SaveChanges();
+            }
+
+            public void UpdateData(int number, Competitor comp)
+            {
+                  var oldNumber = FindOne(number);
+
+                  oldNumber.Number = comp.Number;
+                  oldNumber.Name = comp.Name;
+                  oldNumber.Nationality = comp.Nationality;
+                  oldNumber.Gender = comp.Gender;
+                  oldNumber.Height = comp.Height;
+                  oldNumber.Weight = comp.Weight;
+                  oldNumber.AchivedPlace = comp.AchivedPlace;
+                  oldNumber.Category = comp.Category;
+                  oldNumber.Sponsor = comp.Sponsor;
+                  oldNumber.CategoryId = comp.CategoryId;
+                  oldNumber.SponsorId = comp.SponsorId;
+                  SaveAll();
+
+            }
+      }
+}
---
+++ BWJ4AO_FELEVES_FELADAT/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+      public interface IRepository<T> where T : new()
+      {
+            void AddNewItem(T item);
+            void Delete(T item);
+            void Delete(int number);
+            T FindOne(int number);
+            IQueryable<T> ListAll();
+            void UpdateData(int number, T item);
+            void SaveAll();
+      }
+}
