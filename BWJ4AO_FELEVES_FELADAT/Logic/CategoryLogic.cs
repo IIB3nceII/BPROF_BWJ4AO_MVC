217cf0a73be979396c4db1a1e11380890d36212e
Bence Papp <bencepapp1101@gmail.com>
2020-11-19 14:01:11 +0100

    CategoryLogic elkészítve

---
+++ BWJ4AO_FELEVES_FELADAT/Logic/CategoryLogic.cs
@@ -0,0 +1,48 @@
+﻿using Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+      class CategoryLogic
+      {
+            IRepository<Competitor> comprepo;
+            IRepository<Category> categrepo;
+            IRepository<Sponsor> sponsrepo;
+
+            public CategoryLogic(IRepository<Competitor> comprepo, IRepository<Category> categrepo, IRepository<Sponsor> sponsrepo)
+            {
+                  this.comprepo = comprepo;
+                  this.categrepo = categrepo;
+                  this.sponsrepo = sponsrepo;
+            }
+
+            public void Add(Category c)
+            {
+                  this.categrepo.AddNewItem(c);
+            }
+
+            public void Delete(string id)
+            {
+                  this.categrepo.Delete(id);
+            }
+
+            public IQueryable<Category> List()
+            {
+                  return categrepo.ListAll();
+            }
+
+            public Category Find(string id)
+            {
+                  return categrepo.FindOne(id);
+            }
+
+            public void Update(string id, Category c)
+            {
+                  categrepo.UpdateData(id, c);
+            }
+      }
+}
