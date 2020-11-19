bfb6873094e322f2574999ec0a45e3b09cb66a4a
Bence Papp <bencepapp1101@gmail.com>
2020-11-19 13:56:38 +0100

    Logic réteg létrehozva, CompetitorLogic elkészítve

---
+++ BWJ4AO_FELEVES_FELADAT/Logic/CompetitorLogic.cs
@@ -0,0 +1,46 @@
+﻿using Models;
+using Repository;
+using System;
+using System.Linq;
+
+namespace Logic
+{
+      public class CompetitorLogic
+      {
+            IRepository<Competitor> comprepo;
+            IRepository<Category> categrepo;
+            IRepository<Sponsor> sponsrepo;
+
+            public CompetitorLogic(IRepository<Competitor> comprepo, IRepository<Category> categrepo, IRepository<Sponsor> sponsrepo)
+            {
+                  this.comprepo = comprepo;
+                  this.categrepo = categrepo;
+                  this.sponsrepo = sponsrepo;
+            }
+
+            public void Add(Competitor c)
+            {
+                  this.comprepo.AddNewItem(c);
+            }
+
+            public void Delete(string id)
+            {
+                  this.comprepo.Delete(id);
+            }
+
+            public IQueryable<Competitor> List()
+            {
+                  return comprepo.ListAll();
+            }
+
+            public Competitor Find(string id)
+            {
+                  return comprepo.FindOne(id);
+            }
+
+            public void Update(string id, Competitor c)
+            {
+                  comprepo.UpdateData(id, c);
+            }
+      }
+}
