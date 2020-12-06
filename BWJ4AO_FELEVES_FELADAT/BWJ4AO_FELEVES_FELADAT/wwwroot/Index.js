8b6820eb8024cc84253248bb5f6edb7fcb42db82
Bence Papp <bencepapp1101@gmail.com>
2020-12-06 16:35:58 +0100

    Index chtml css js fileok létrehozva és frissítve

---
+++ BWJ4AO_FELEVES_FELADAT/BWJ4AO_FELEVES_FELADAT/wwwroot/Index.js
@@ -0,0 +1,20 @@
+﻿const selectElement = (s) => document.querySelector(s);
+
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
+
+window.onscroll = function () { scroll() };
+
+function scroll() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("nav").style.backgroundColor = "#00000066";
+        document.getElementById("nav").style.borderRadius = "30px";
+    } else {
+        document.getElementById('nav').style.removeProperty("background-color");
+    }
+}
