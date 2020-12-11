1203858b8f81847b24d4ba694b9f4e10330d6260
Bence Papp <bencepapp1101@gmail.com>
2020-12-11 14:18:16 +0100

    AddSponsor.cshtml front end elkészítve

---
+++ BWJ4AO_FELEVES_FELADAT/BWJ4AO_FELEVES_FELADAT/wwwroot/AddSponsor.js
@@ -0,0 +1,21 @@
+﻿//navbar
+const selectElement = (s) => document.querySelector(s);
+
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
+
+window.onscroll = function () { scroll() };
+
+function scroll() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header").style.backgroundColor = " #000000b3";
+        //document.getElementById("header").style.borderRadius="30px";
+    } else {
+        document.getElementById('header').style.removeProperty("background-color");
+    }
+}
