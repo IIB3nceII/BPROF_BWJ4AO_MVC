20107efd4488cbef7b104288d0ee547c8cff133b
Bence Papp <bencepapp1101@gmail.com>
2021-04-17 10:53:58 +0200

    Create SponsorController

--- BWJ4AO_FELEVES_FELADAT/ApiApp/Controllers/SponsorController.cs
+++ BWJ4AO_FELEVES_FELADAT/ApiApp/Controllers/SponsorController.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ApiApp.Controllers
 {
-      public class SponsorController
+      [ApiController]
+      [Route("{controller}")]
+      public class SponsorController : ControllerBase
       {
+            SponsorLogic logic;
+
+            public SponsorController(SponsorLogic logic)
+            {
+                  this.logic = logic;
+            }
+
+            [HttpDelete("{uid}")]
+            public void DeleteSponsor(string uid)
+            {
+                  logic.Delete(uid);
+            }
+
+            [HttpGet("{uid}")]
+            public Sponsor GetSponsor(string uid)
+            {
+                  return logic.Find(uid);
+            }
+
+            [HttpGet]
+            public IEnumerable<Sponsor> GetAllSponsor()
+            {
+                  return logic.List();
+            }
+
+            [HttpPost]
+            public void AddSponsor([FromBody] Sponsor item)
+            {
+                  logic.Add(item);
+            }
+
+            [HttpPut("{oldid}")]
+            public void UpdateSponsor(string oldid, [FromBody] Sponsor item)
+            {
+                  logic.Update(oldid, item);
+            }
       }
 }
