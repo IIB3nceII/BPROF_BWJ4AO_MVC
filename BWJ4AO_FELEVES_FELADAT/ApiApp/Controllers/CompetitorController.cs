f6cc20e095193db618e41a6e59156f4f979be28c
Bence Papp <bencepapp1101@gmail.com>
2021-04-17 10:52:15 +0200

    Create CompetitorController

--- BWJ4AO_FELEVES_FELADAT/ApiApp/Controllers/CompetitorController.cs
+++ BWJ4AO_FELEVES_FELADAT/ApiApp/Controllers/CompetitorController.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ApiApp.Controllers
 {
-      public class CompetitorController
+      [ApiController]
+      [Route("{controller}")]
+      public class CompetitorController : ControllerBase
       {
+            CompetitorLogic logic;
+
+            public CompetitorController(CompetitorLogic logic)
+            {
+                  this.logic = logic;
+            }
+
+            [HttpDelete("{uid}")]
+            public void DeleteCompetitor(string uid)
+            {
+                  logic.Delete(uid);
+            }
+
+            [HttpGet("{uid}")]
+            public Competitor GetCompetitor(string uid)
+            {
+                  return logic.Find(uid);
+            }
+
+            [HttpGet]
+            public IEnumerable<Competitor> GetAllCompetitor()
+            {
+                  return logic.List();
+            }
+
+            [HttpPost]
+            public void AddCompetitor([FromBody] Competitor item)
+            {
+                  logic.Add(item);
+            }
+
+            [HttpPut("{oldid}")]
+            public void UpdateCompetitor(string oldid, [FromBody] Competitor item)
+            {
+                  logic.Update(oldid, item);
+            }
       }
 }
