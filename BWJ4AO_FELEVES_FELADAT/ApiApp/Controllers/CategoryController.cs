4dd9573b8a15fa05b449af721746276df070741a
Bence Papp <bencepapp1101@gmail.com>
2021-04-17 10:50:01 +0200

    Create CategoryController

---
+++ BWJ4AO_FELEVES_FELADAT/ApiApp/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiApp.Controllers
+{
+      [ApiController]
+      [Route("{controller}")]
+      public class CategoryController : ControllerBase
+      {
+            CategoryLogic logic;
+
+            public CategoryController(CategoryLogic logic)
+            {
+                  this.logic = logic;
+            }
+
+            [HttpDelete("{uid}")]
+            public void DeleteVideo(string uid)
+            {
+                  logic.Delete(uid);
+            }
+
+            [HttpGet("{uid}")]
+            public Category GetVideo(string uid)
+            {
+                  return logic.Find(uid);
+            }
+
+            [HttpGet]
+            public IEnumerable<Category> GetAllVideo()
+            {
+                  return logic.List();
+            }
+
+            [HttpPost]
+            public void AddCategory([FromBody] Category item)
+            {
+                  logic.Add(item);
+            }
+
+            [HttpPut("{oldid}")]
+            public void UpdateCategory(string oldid, [FromBody] Category item)
+            {
+                  logic.Update(oldid, item);
+            }
+      }
+}
--- BWJ4AO_FELEVES_FELADAT/ApiApp/Startup.cs
+++ BWJ4AO_FELEVES_FELADAT/ApiApp/Startup.cs
@@ -20,13 +20,11 @@ namespace ApiApp
 
             public IConfiguration Configuration { get; }
 
-            // This method gets called by the runtime. Use this method to add services to the container.
             public void ConfigureServices(IServiceCollection services)
             {
-                  services.AddRazorPages();
+                  services.AddControllers();
             }
 
-            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
             public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
             {
                   if (env.IsDevelopment())
@@ -36,7 +34,6 @@ namespace ApiApp
                   else
                   {
                         app.UseExceptionHandler("/Error");
-                        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                         app.UseHsts();
                   }
 
@@ -49,7 +46,7 @@ namespace ApiApp
 
                   app.UseEndpoints(endpoints =>
                   {
-                        endpoints.MapRazorPages();
+                        endpoints.MapControllers();
                   });
             }
       }
