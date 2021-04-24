10b6a08bafc51de586fc64b2790cd42cf860bc78
Bence Papp <bencepapp1101@gmail.com>
2021-04-24 12:42:04 +0200

    Home modified

--- mro-webapp/src/components/Header.tsx
+++ mro-webapp/src/components/Header.tsx
@@ -4,7 +4,7 @@ import OlympiaLogo from "../assets/olympialogo.png";
 
 function Header() {
   return (
-    <div className="sticky top-0 bg-gray-200 h-24 flex justify-between">
+    <header className="sticky top-0 bg-gray-50 h-24 flex justify-between ms:justify-between">
       <div className="flex p-6 items-center">
         <MenuIcon className="h-10 text-gray-500 cursor-pointer transition duration-100 transform" />
       </div>
@@ -19,7 +19,7 @@ function Header() {
       <div className="flex items-center p-6 ">
         <p className="ml-10 text-lg text-gray-500 cursor-pointer">Sign in</p>
       </div>
-    </div>
+    </header>
   );
 }
 
--- mro-webapp/src/components/Home/Home.tsx
+++ mro-webapp/src/components/Home/Home.tsx
@@ -1,11 +1,112 @@
-import React from 'react';
+import React from "react";
+import "./Home.scss";
+import HomeBg from "../../assets/2020-Olympia-bg.jpg";
+import ArnoldPic from "../../assets/ArnoldProfilePic.jpg";
+import { ScaleIcon } from "@heroicons/react/solid";
 
 function Home() {
-    return (
-        <div className="home">
-            <h1>Hello</h1>
+  return (
+    <div className="bg-gray-50 items-center h-screen">
+      <div
+        className="  h-4/6 bg-local bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url('${HomeBg}')` }}
+      >
+        <div className="bg-red-900 bg-opacity-40 h-full">
+          <div className="w-8/12 h-full ml-auto mr-auto text-white text-left flex-wrap">
+            <h3 className="text-3xl pt-28 ml-2">Welcome to the</h3>
+            <h1 className="text-8xl mb-10">Mr. Olympia</h1>
+            <p className="w-1/2 text-lg">
+              Mr. Olympia is the title awarded to the winner of the professional
+              men's bodybuilding contest at Joe Weider's Olympia Fitness &
+              Performance Weekend—an international bodybuilding competition that
+              is held annually by the International Federation of BodyBuilding &
+              Fitness (IFBB).
+            </p>
+          </div>
         </div>
-    )
+      </div>
+
+      <div className="items-center text-center h-full bg-gray-50">
+        <div className="w-8/12 ml-auto mr-auto ">
+          <div className="mt-10 mb-28">
+            <h3 className="text-gray-500 text-3xl font-medium">Check</h3>
+            <h1 className="text-gray-500 text-4xl font-medium">
+              The Mr. Olympia Categories
+            </h1>
+          </div>
+
+          <div className="grid grid-cols-3 gap-4">
+            <div className="flex-col h-56 bg-red-600 bg-opacity-50 shadow-2xl rounded-lg">
+              <h1 className="m-5 text-4xl font-medium">Men’s Open</h1>
+              <p className="m-5 text-lg font-medium">
+                All the top players participate here with their weights starting
+                from 300 lbs or 137 kgs.
+              </p>
+            </div>
+
+            <div className="flex-col h-56 bg-red-600 bg-opacity-50 shadow-2xl rounded-lg">
+              <h1 className="m-5 text-4xl font-medium">Women's Bikini</h1>
+              <p className="m-5 text-lg font-medium">
+                The IFBB WOS Ms. Olympia is both the highest ranking
+                professional female bodybuilding competition and the title of
+                the winner of the competition.
+              </p>
+            </div>
+
+            <div className="flex-col h-56 bg-red-600 bg-opacity-50 shadow-2xl rounded-lg">
+              <h1 className="m-5 text-4xl font-medium">Classic Physique</h1>
+              <p className="m-5 text-lg font-medium">
+                This division is targeted to men who want to build their muscles
+                and size above the normal Men’s Physique limits (discussed
+                later), but not to the extent of Open Division Bodybuilding.
+              </p>
+            </div>
+
+            <div className="flex-col h-56 bg-red-600 bg-opacity-50 shadow-2xl rounded-lg">
+              <h1 className="m-5 text-4xl font-medium">Men’s Physique</h1>
+              <p className="m-5 text-lg font-medium">
+                The judges pay close attention to Athletic, Less Muscularity and
+                an Aesthetic physique.
+              </p>
+            </div>
+
+            <div className="flex-col h-56 bg-red-600 bg-opacity-50 shadow-2xl rounded-lg">
+              <h1 className="m-5 text-4xl font-medium">Women’s Physique</h1>
+              <p className="m-5 text-lg font-medium">
+                Competitors should display a toned, athletic physique showcasing
+                femininity, muscle tone, beauty/flow of physique.
+              </p>
+            </div>
+
+            <div className="flex-col h-56 bg-red-600 bg-opacity-50 shadow-2xl rounded-lg">
+              <h1 className="m-5 text-4xl font-medium">Mr. Olympia</h1>
+              <p className="m-5 text-lg font-medium">
+                The requirements for this class, just as the name implies, is
+                that the athlete needs to weigh up to and under 212 Lb (i.e.,
+                96.6 kg).
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+        <div>
+            <div>
+                <div>
+                <ScaleIcon className="h-32" />
+                <p>“Not many people understand what a pump is. It must be experienced to be understood. It is the greatest feeling that I get. I search for this pump because it means that that my muscles will grow when I get it. I get a pump when the blood is running into my muscles. They become really tight with blood. Like the skin is going to explode any minute. It’s like someone putting air in my muscles. It blows up. It feels fantastic.”</p>
+                </div>
+
+                <div>
+                <div className="">
+                    <img className="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src={ArnoldPic} alt="Picture of Arnold Schwarzenegger" loading="lazy" />
+                    </div>
+                    <h1>Arnold Schwarzenegger <br />(7 Mr Olympia wins)</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
 }
 
 export default Home;
